d39dd2fb697f7d6592e79cf4f3c21c6743de4b8f
Laura <55337216+Laurilith@users.noreply.github.com>
2020-01-28 16:27:42 -0400

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,34 @@
+package assns1.lmh;
+
+final class Grade {
+	private String courseNum;
+	private int CH;
+	private double GPA;
+	private char letterGrade;
+	
+	public Grade(String num, int H, double GP, char letter) {
+		courseNum=num;
+		CH=H;
+		GPA=GP;
+		letterGrade=letter;
+		
+	}
+	public int getCH() {
+		return CH;
+	}	
+	public double getGPA() {
+		return GPA;
+	}
+	public String getcourseNum() {
+		return courseNum;
+	}
+	public char getLetterGrade() {
+		return letterGrade;
+	}
+	@Override
+	public String toString() {
+		String ret;
+		ret=courseNum+" "+CH+"CH "+GPA+" ("+letterGrade+")\n";
+		return ret;
+	}
+}
